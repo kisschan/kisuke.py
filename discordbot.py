82a35c7d6b7f4f384cea19ac428ff36cada30fdc
kisschan <goldentakeshi1023@gmail.com>
2021-09-22 00:26:10 +0900

    4th fixed ryuusuke 2

--- discordbot.py
+++ discordbot.py
@@ -15,7 +15,7 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('おうなんかようか？')
-    
+
 @bot.command()
 async def 竜介(ctx):
     await ctx.send('くさい　だろぅな・・')
