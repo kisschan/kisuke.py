0a8bd239518f9351110f91af03df69a953979817
kisschan <goldentakeshi1023@gmail.com>
2021-09-22 02:59:02 +0900

    fixed indent2

--- cogs/discord_message_reply.py
+++ cogs/discord_message_reply.py
@@ -1,23 +1,23 @@
 from discord.ext import commands  # Bot Commands Frameworkをインポート
 import discord
 
-    class Message_RepeyCog(commands.Cog):
-        def __init__(self, bot):
-            self.bot = bot
-            self._last_member = None
- 
-        @commands.Cog.listener()
-        # Cogが読み込まれた時に発動
-        async def on_ready(self):
-            print('Message_RepeyCog on ready!')
- 
-        @bot.command()
-        async def ping(ctx):
-            await ctx.send('おうなんかようか？')
+class Message_RepeyCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
 
-        @bot.command()
-        async def 竜介(ctx):
-            await ctx.send('くさい　だろぅな・・')
- 
+    @commands.Cog.listener()
+    # Cogが読み込まれた時に発動
+    async def on_ready(self):
+        print('Message_RepeyCog on ready!')
+
+    @bot.command()
+    async def ping(ctx):
+        await ctx.send('おうなんかようか？')
+
+    @bot.command()
+    async def 竜介(ctx):
+        await ctx.send('くさい　だろぅな・・')
+
     def setup(bot):
-    bot.add_cog(Message_RepeyCog(bot)) # Message_RepeyCogにBotを渡してインスタンス化し、Botにコグとして登録する
+        bot.add_cog(Message_RepeyCog(bot))  # Message_RepeyCogにBotを渡してインスタンス化し、Botにコグとして登録する
