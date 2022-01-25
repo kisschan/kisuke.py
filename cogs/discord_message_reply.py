3ed4f48b14fe5201b254d027543332e6e2c2c694
kisschan <goldentakeshi1023@gmail.com>
2022-01-26 05:01:26 +0900

    配信者の数を取れるように

--- cogs/discord_message_reply.py
+++ cogs/discord_message_reply.py
@@ -28,15 +28,17 @@ class Message_RepeyCog(commands.Cog):
         """ぎこぽいの配信者を取得。部屋まで飛べる"""
         texts = ['http://gikopoipoi.net  で配信中']
         texts.extend([datetime.now().strftime('%Y{0}%-m{1}%-d{2} %-H{3}%-M{4}%-S{5}').format(*'年月日時分秒')])
+        streamNum = 0
         url = 'https://gikopoipoi.net/areas/gen/streamers'
         texts.extend(["-"])
         req = urllib.request.Request(url)
         with urllib.request.urlopen(req) as res:
             body = json.load(res)
         for item in body:
+            streamNum = streamNum + len(item['streamers'])
             texts.extend([i if i else '名無しさん' for i in item['streamers']])
             texts.extend(['https://gikopoipoi.net/?areaid=gen&roomid=' + item['id']])
-        texts.insert(2, str(len(item)) + "名が配信中")
+        texts.insert(2, "(" + str(streamNum) + "名が配信中)")
         await ctx.send(('\n'.join(texts)))
 
 def setup(bot):
